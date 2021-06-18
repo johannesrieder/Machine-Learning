d4e3a7cabcc210197f6237c754f6a6447a358aa9
Marius J. Bauer <54984308+mariusjbauer@users.noreply.github.com>
2021-06-18 10:24:04 +0200

    train/test

--- R_Template_Aufgaben_5.R
+++ R_Template_Aufgaben_5.R
@@ -1,16 +1,14 @@
 #
-# Template f?r die Bearbeitung der ?bungsaufgaben 5
+# Template für die Bearbeitung der Übungsaufgaben 5
 #        -- Analyse des Caravan-Datensatz -- 
 #
 
 #-----------------
-# Bitte ausf?llen:
+# Bitte ausfüllen:
 #-----------------
-# Gruppenname: Obi Wan 
-# Gruppenteilnehmer:
+# Gruppenname: Obi-Wan
+# Gruppenteilnehmer: Johannes Rieder, Jannik Steck, Lisa Trovato-Monastra, Marius Bauer
 
-
-#test
 
 #-------------------------
 # notwendinge Bibliotheken
@@ -22,24 +20,79 @@ library(class)  # knn Funktion
 #------------------------------------
 # Aufgabe 1: Trainings- und Testdaten
 #------------------------------------
+summary(Caravan)
+data <- ISLR::Caravan
+x <- data$Purchase
+y <- data$ABYSTAND
 
-#    ...Platz f?r Ihren Code...
+set.seed(99)
+size <- sample(length(data),0.8*length(data))
+
+train           <- as.data.frame(data[size,c("purchase")])
+test            <- as.data.frame(data[-size,c("purchase")])
+train.classes   <- data[size, "default"]
+test.classes    <- data[-size, "default"]
+
 
 #------------------------------------
 # Aufgabe 2: Logistische Regression
 #------------------------------------
+glm.fit <- glm(
+  data    = data.train,
+  formula = y ~ .,
+  family  = "binomial"
+)
 
-#    ...Platz f?r Ihren Code...
+summary(glm.fit)
+
+# Cut-Off-Point t=0.5:
+# Steigung = -a1/a2
+# y-Achsenabschnitt = -a0/a2
+steigung         <-  - glm.fit$coef["x1"] / glm.fit$coef["x2"]  
+yAchsenAbschnitt <-  - glm.fit$coef["(Intercept)"] / glm.fit$coef["x2"]
+
+# Plot 
+plot(data1, col="red",ylim=c(-100,150))
+points(data2, col="green")
+abline(a=yAchsenAbschnitt, b=steigung, col="blue")
+
+# Plot (nur Testdaten)
+plot(subset(test,y==0,select=x1:x2), col="red",ylim=c(-100,150))
+points(subset(test,y==1,select=x1:x2), col="green")
+abline(a=yAchsenAbschnitt, b=steigung, col="blue")
+
+
+## Confusion-Matrix (Testdaten)
+test.glm.predictedProbabilities <- predict( 
+  object  = glm.fit, 
+  newdata = data.test, 
+  type    = "response"
+)
+
+test.glm.prediction      <- ifelse(test.glm.predictedProbabilities < 0.5, 0, 1)  
+test.glm.confusionMatrix <- table(test.glm.prediction,data.test$y)
+test.glm.confusionMatrix
+
+## Confusion-Matrix (Trainingsdaten)
+train.glm.predictedProbabilities <- predict( 
+  object  = glm.fit, 
+  newdata = data.train, 
+  type    = "response"
+)
+
+train.glm.prediction <- ifelse(train.glm.predictedProbabilities < 0.5, 0, 1)  
+train.glm.confusionMatrix <- table(train.glm.prediction,data.train$y)
+train.glm.confusionMatrix
+
 
 #------------------------------------
 # Aufgabe 3: KNN
 #------------------------------------
 
-#    ...Platz f?r Ihren Code...
+#    ...Platz für Ihren Code...
 
 #------------------------------------
 # Aufgabe 4: Modellauswahl
 #------------------------------------
 
-#    ...Platz f?r Ihren Code, Auswahl und Begr?ndung...
-
+#    ...Platz für Ihren Code, Auswahl und Begründung...
