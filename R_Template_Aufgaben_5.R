57a3dae48dba3e6727e851a5d7cdd0d2e8b8d266
Marius J. Bauer <54984308+mariusjbauer@users.noreply.github.com>
2021-06-18 15:47:10 +0200

    + knn

--- R_Template_Aufgaben_5.R
+++ R_Template_Aufgaben_5.R
@@ -1,6 +1,6 @@
 #
 # Template für die Bearbeitung der Übungsaufgaben 5
-#        -- Analyse des Caravan-Datensatz -- 
+#        -- Analyse des Caravan-Datensatz --
 #
 
 #-----------------
@@ -23,20 +23,19 @@ library(class)  # knn Funktion
 data <- ISLR::Caravan
 
 set.seed(42)
-rows.train <- sample(length(data$Purchase),0.8*length(data$Purchase))
+rows.train <-
+  sample(length(data$Purchase), 0.8 * length(data$Purchase))
 
-train         <- as.data.frame(data[rows.train,])
-test          <- as.data.frame(data[-rows.train,])
+train         <- as.data.frame(data[rows.train, ])
+test          <- as.data.frame(data[-rows.train, ])
 
 #------------------------------------
 # Aufgabe 2: Logistische Regression
 #------------------------------------
 #Logistische Regression mit Output-Variable Purchase und den anderen Variablen als Input-Variablen
-glm.fit <- glm(
-  data    = train,
-  formula = Purchase ~ .,
-  family  = "binomial"
-)
+glm.fit <- glm(data    = train,
+               formula = Purchase ~ .,
+               family  = "binomial")
 
 summary(glm.fit)
 
@@ -45,40 +44,55 @@ t <- c(0.15, 0.35, 0.55)
 
 
 ## Vorhersage der Wahrscheinlichkeiten der Testdaten auf Basis der logistischen Regression
-test.glm.predictedProbabilities <- predict( 
-  object  = glm.fit, 
-  newdata = test, 
-  type    = "response"
-)
+glm_predprob <- predict(object  = glm.fit,
+                        newdata = test,
+                        type    = "response")
 
 # For-Schleife erzeugt Confusion-Matrizen anhand der drei Cut-Off Points
 for (p in t) {
   print(p)
-  test.glm.prediction      <- ifelse(test.glm.predictedProbabilities <= p, 0, 1)  
-  test.glm.confusionMatrix <- table(test.glm.prediction,test$Purchase)
-  print(test.glm.confusionMatrix)
+  glm_pred      <- ifelse(glm_predprob <= p, 0, 1)
+  lr_confusionmatrix <- table(glm_pred, test$Purchase)
+  print(lr_confusionmatrix)
 }
 
 #------------------------------------
 # Aufgabe 3: KNN
 #------------------------------------
 
-# Drei verschiedene K für KNN
-train         <- as.data.frame(data[rows.train, seq(1, length(train)-1, 1)])
-test          <- as.data.frame(data[-rows.train,])
-train.classes <- data[rows.train, seq(1, length(train)-1, 1)]
-test.classes  <- data[-rows.train, "Purchase"]
+# Skalierung notwendig, da 85 unterschiedliche Input-Variablen
+# Purchase wird von Trainingsdaten entfernt
+knn_train         <-
+  as.data.frame(scale(data[rows.train, seq(0, length(train) - 1, 1)]))
+# Purchase wird von Testdaten entfernt
+knn_test          <-
+  as.data.frame(scale(data[-rows.train, seq(0, length(test) - 1, 1)]))
+# Label der Trainingsdaten für Purchase-Klassifikation als Vektor
+train_label       <- train[['Purchase']]
+# Label der Testdaten für Purchase-Klassifikation als Vektor
+test_label        <- test[['Purchase']]
+
+
+# Drei verschiedene K für knn-Funktion
 k <- c(1, 3, 5)
 
-knn <- knn(
-  train = train,         ## Input-Variablen der Trainingsdaten      
-  test  = test,          ## Input-Variablen der Testdaten
-  cl    = train.classes, ## Class-Label der Trainingsdaten
-  k     = 3,
-  prob = TRUE
-) 
-
-confusion.table.k3 <- table(knn, train.classes)
+# Anwendung knn-Funktion mit drei unterschiedlichen k
+for (i in k) {
+  knn <- knn(
+    ## Input-Variablen der Trainingsdaten
+    train = knn_train,
+    ## Input-Variablen der Testdaten
+    test  = knn_test,
+    ## Class-Label der Trainingsdaten
+    cl    = train_label,
+    k     = i,
+    prob = TRUE
+  )
+  # Erzeugung und Ausgabe der Confusion Matrix
+  knn_confusionmatrix <- table(knn, test_label)
+  print(paste("Confusion Matrix (k =", i, ")"))
+  print(knn_confusionmatrix)
+}
 
 
 #------------------------------------
