7b6836ec5df25d38c5ebb2e4788364e3e258dccf
Marius J. Bauer <54984308+mariusjbauer@users.noreply.github.com>
2021-06-18 18:35:46 +0200

    matrizen werden in list gespeichert und bei A4 automatisch aufgerufen; A4 listet alle Matrizen + Precisions auf (ob Rechnung stimmt muss man nochmal schauen)

--- R_Template_Aufgaben_5.R
+++ R_Template_Aufgaben_5.R
@@ -16,18 +16,19 @@
 library(ISLR)   # Datensatz
 library(class)  # knn Funktion
 
-
 #------------------------------------
 # Aufgabe 1: Trainings- und Testdaten
 #------------------------------------
 data <- ISLR::Caravan
 
-set.seed(42)
+set.seed(43)
 rows.train <-
   sample(length(data$Purchase), 0.8 * length(data$Purchase))
 
 train         <- as.data.frame(data[rows.train,])
 test          <- as.data.frame(data[-rows.train,])
+
+matrices <- list()
 
 #------------------------------------
 # Aufgabe 2: Logistische Regression
@@ -47,12 +48,12 @@ glm_predprob <- predict(object  = glm.fit,
 
 # For-Schleife erzeugt Confusion-Matrizen anhand der drei Cut-Off Points
 for (p in t) {
-  print(p)
   glm_pred      <- ifelse(glm_predprob <= p, 0, 1)
-  lr_confusionmatrix <- table(glm_pred, test$Purchase)
-  assign(paste("lr_confusionmatrix", p, sep = ""), lr_confusionmatrix)
-  print(paste("Confusion Matrix (t =", p, ")"))
+  lr_confusionmatrix = table(glm_pred, test$Purchase)
+  matrices[[length(matrices)+1]] <- list(paste("lr_confusionmatrix_t", p, sep = ""), lr_confusionmatrix)
+  print(paste("Confusion Matrix ( t =", p, ")"))
   print(lr_confusionmatrix)
+  
 }
 
 #------------------------------------
@@ -60,14 +61,18 @@ for (p in t) {
 #------------------------------------
 
 # Skalierung notwendig, da 85 unterschiedliche Input-Variablen
+# Input-Variablen ohne Output-Variable
+invar <- seq(0, length(train) - 1, 1)
+
 # Purchase wird von Trainingsdaten entfernt
-knn_train <-
-  as.data.frame((data[rows.train, seq(0, length(train) - 1, 1)]))
+knn_train <- train[, invar]
+
 # Purchase wird von Testdaten entfernt
-knn_test  <-
-  as.data.frame((data[-rows.train, seq(0, length(test) - 1, 1)]))
+knn_test  <- test[, invar]
+
 # Label der Trainingsdaten für Purchase-Klassifikation als Vektor
 train_label <- train[['Purchase']]
+
 # Label der Testdaten für Purchase-Klassifikation als Vektor
 test_label  <- test[['Purchase']]
 
@@ -77,17 +82,16 @@ k <- c(1, 3, 5)
 # Anwendung knn-Funktion mit drei unterschiedlichen k
 for (i in k) {
   knn <- knn(
-    train = knn_train,
-    test = knn_test,
+    train = scale(knn_train),
+    test = scale(knn_test),
     cl = train_label,
     k = i,
     prob = TRUE
   )
   # Erzeugung und Ausgabe der Confusion Matrix
-  knn_confusionmatrix <- table(knn, test_label)
-  assign(paste("knn_confusionmatrix", i, sep = ""),
-         knn_confusionmatrix)
-  print(paste("Confusion Matrix (k =", i, ")"))
+  knn_confusionmatrix = table(knn, test_label)
+  matrices[[length(matrices)+1]] <- list(paste("knn_confusionmatrix_k", i, sep = ""), lr_confusionmatrix)
+  print(paste("Confusion Matrix ( k =", i, ")"))
   print(knn_confusionmatrix)
 }
 
@@ -98,5 +102,27 @@ for (i in k) {
 
 # Summary der Confusion Matrices
 
+for (matrix in matrices) {
+  m = matrix[[2]]
+  
+  tn = m[1]
+  fp = m[2]
+  fn = m[3]
+  tp = m[4]
+
+  print("==============================================================")
+  print(matrix[[1]])
+  print("--------------------------------------------------------------")
+  print(paste("Positives / Negatives: ", tp+fn, "/", fp+tn))
+  prec_t <- (tp+tn)/(tp+fp+fn+tn)
+  print(paste("Average Precision: ", prec_t))
+  prec_tp <- tp/(tp+fn)
+  print(paste("Precision (Positives): ", prec_tp))
+  prec_tn <- tn/(fp+tn)
+  print(paste("Precision (Negatives): ", prec_tn))
+  print("--------------------------------------------------------------")
+  print(paste("TN (1,1) / FP (1,2) / FN (2,1) / TP (2,2): ", tn, "/", fp, "/", fn, "/", tp))
+  print(m)
+}
 
 #    ...Platz für Ihren Code, Auswahl und Begründung...
