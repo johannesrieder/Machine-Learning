b8a7e64576f9c72be02f53f076ffb60dcd2ec572
Johannes Rieder <54912990+johannesrieder@users.noreply.github.com>
2021-06-18 09:55:41 +0200

    Template upload

---
+++ R_Template_Aufgaben_5.R
@@ -0,0 +1,44 @@
+#
+# Template für die Bearbeitung der Übungsaufgaben 5
+#        -- Analyse des Caravan-Datensatz -- 
+#
+
+#-----------------
+# Bitte ausfüllen:
+#-----------------
+# Gruppenname:
+# Gruppenteilnehmer: 
+
+
+
+#-------------------------
+# notwendinge Bibliotheken
+#-------------------------
+library(ISLR)   # Datensatz
+library(class)  # knn Funktion
+
+
+#------------------------------------
+# Aufgabe 1: Trainings- und Testdaten
+#------------------------------------
+
+#    ...Platz für Ihren Code...
+
+#------------------------------------
+# Aufgabe 2: Logistische Regression
+#------------------------------------
+
+#    ...Platz für Ihren Code...
+
+#------------------------------------
+# Aufgabe 3: KNN
+#------------------------------------
+
+#    ...Platz für Ihren Code...
+
+#------------------------------------
+# Aufgabe 4: Modellauswahl
+#------------------------------------
+
+#    ...Platz für Ihren Code, Auswahl und Begründung...
+
